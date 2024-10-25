1bfb3379c4740eb1a8a93eb05af66028640a6004
Robert Schöne <rschoene@users.noreply.github.com>
2024-10-25 18:59:58 +0200

    Hopefully last update, uses Windows button layout now

--- walk_man/walk-man.py
+++ walk_man/walk-man.py
@@ -8,6 +8,8 @@ pygame.mixer.init()
 
 crouch_sound = pygame.mixer.Sound("../sounds/crouch.wav")
 jump_sound = pygame.mixer.Sound("../sounds/jump.wav")
+getup_sound = pygame.mixer.Sound("../sounds/getup.wav")
+hit_sound = pygame.mixer.Sound("../sounds/hit.wav")
 
 left_sound = pygame.mixer.Sound("../sounds/left.wav")
 right_sound = pygame.mixer.Sound("../sounds/right.wav")
@@ -30,15 +32,14 @@ def main():
 
     # Sounds for buttons
     button_actions = {
-        0 : crouch_sound,
-        1 : crouch_sound,
-        2 : jump_sound,
-        3 : jump_sound,
-        11 : left_sound,
-        12 : right_sound,
-        13 : forward_sound,
-        14 : back_sound,
+        0 : jump_sound, # 0 is A under Windows
+        2 : hit_sound, # 2 is X under Windows
     }
+    # Sounds looped on button
+    button_loop_actions = {
+        1 : [crouch_sound,getup_sound], # 1 is B under Windows
+    }
+    
 
     done = False
     while not done:
@@ -53,6 +54,18 @@ def main():
                     if action == event.button:
                         if button_actions[action].get_num_channels() == 0:
                             button_actions[action].play()
+
+                for action in button_loop_actions:
+                    if action == event.button:
+                        if button_loop_actions[action][0].get_num_channels() == 0:
+                            button_loop_actions[action][0].play(loops=-1)
+
+            if event.type == pygame.JOYBUTTONUP:
+                print("Joystick button released.", event.button)
+                for action in button_loop_actions:
+                    if action == event.button:
+                         button_loop_actions[action][0].stop()
+                         button_loop_actions[action][1].play()
 
             # Handle hotplugging
             if event.type == pygame.JOYDEVICEADDED:
@@ -71,26 +84,48 @@ def main():
 
             axes = joystick.get_numaxes()
 
-            for i in range(axes):
+            # we are only interested in axis 0 and 1 (left stick)
+            # 2,3 would be right stick and 4,5 would be LT/RT
+            for i in [0,1]:
                 axis = joystick.get_axis(i)
+                #if (axis > 0.5 or axis < -0.5): # and (i == 0 or i == 1) :
+                #    print("axis ",i,axis)
                 if i == 0:
-                    if axis > 0.3:
+                    if axis > 0.5:
                         if right_sound.get_num_channels() == 0:
                             right_sound.play()
-                    if axis < -0.3:
+                    if axis < -0.5:
                         if left_sound.get_num_channels() == 0:
                             left_sound.play()
                 if i == 1:
-                    if axis > 0.3:
+                    if axis > 0.5:
                         if back_sound.get_num_channels() == 0:
                             back_sound.play()
-                    if axis < -0.3:
+                    if axis < -0.5:
                         if forward_sound.get_num_channels() == 0:
                             forward_sound.play()
 
 
-        # Limit to 120 frames per second.
-        clock.tick(120)
+            hats = joystick.get_numhats()
+            for hat_nr in range(1):
+                x, y = joystick.get_hat(hat_nr)
+                #print('hat',hat_nr,x,y)
+                if x == -1:
+                    if left_sound.get_num_channels() == 0:
+                        left_sound.play()
+                if x == 1:
+                        if right_sound.get_num_channels() == 0:
+                            right_sound.play()
+                if y == -1:
+                        if back_sound.get_num_channels() == 0:
+                            back_sound.play()
+                if y == 1:
+                        if forward_sound.get_num_channels() == 0:
+                            forward_sound.play()
+                    
+                
+        # Limit to 60 frames per second.
+        clock.tick(60)
 
 
 if __name__ == "__main__":
